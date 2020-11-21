538bf19b297d89c43f37416f674e40feb4ff533c
ritikamishra01 <manumushra003@gmail.com>
2020-11-21 13:10:54 +0530

    patterns

---
+++ Patterns/pattern01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            if((i+j)%2==0)
+             cout<<"1";
+            else 
+             cout<<"0"; 
+        }
+        cout<<endl;
+    }
+}
